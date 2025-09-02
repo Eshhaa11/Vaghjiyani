a3748e531b967e448bb55689f42606b1cf0140ba
Esha Patel <Patellesha11@gmail.com>
2025-09-02 14:32:11 +0300

    added

--- frontend/src/components/testimonials.jsx
+++ frontend/src/components/testimonials.jsx
@@ -1,3 +1,16 @@
 import react from "react";
 import "../styles/Testimonials.css";
 
+const Testimonials = () => {
+    return (
+    <section className="testimonials" id="testimonials">
+        <h2>What our Clients Say</h2>
+        <blockquote>
+            "Vaghjiyani completed our office project on time and with top-notch quality. Highly recommend!"
+        </blockquote>
+        <cite>- Happy Client</cite>
+    </section>
+    );
+};
+
+export default Testimonials;
