2c87362ec10f184b499df6bfdd096bb6c43fae81
Esha Patel <Patellesha11@gmail.com>
2025-09-02 14:23:05 +0300

    added

---
+++ frontend/src/components/projects.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import "../styles/Projects.css"
+
+const Projects = () => {
+    return (
+        <section className="projects" id="projects">
+            <h2>Our Projects</h2>
+            <p>Take a look at some of our recent work.</p>
+
+        </section>
+    );
+};
+
+export default Projects;
