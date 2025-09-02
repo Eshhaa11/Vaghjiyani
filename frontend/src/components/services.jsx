02a10b510d3b356fe0a68978f63a2332a7e188b6
Esha Patel <Patellesha11@gmail.com>
2025-09-02 14:12:36 +0300

    added

---
+++ frontend/src/components/services.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+import "../styles/Services.css"
+
+const Services = () => {
+    return (
+        <section className="services" id="services">
+            <h2>Our Services</h2>
+            <ul>
+                <li>🏠 Residential Construction</li>
+                
+            </ul>
+
+        </section>
+    )
+}
