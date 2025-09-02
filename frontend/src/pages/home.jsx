4383af69f46b0ff8a50c3c69a6a3aa92b74f434e
Esha Patel <Patellesha11@gmail.com>
2025-09-02 15:00:56 +0300

    added

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+import About from "../components/about";
+import Services from "../components/services";
+import Projects from "../components/projects";
+import Testimonials from "../components/testimonials";
+import Contact from "../components/contacts";
+import Footer from "../components/footer";
+
+
+const Home = () => {
+    return (
+        <>
+        <Navbar />
+        <Hero />
+        <About />
+        <Services />
+        <Projects />
+        <Testimonials />
+        <Contact />
+        <Footer />
+        </>
+    )
+}
