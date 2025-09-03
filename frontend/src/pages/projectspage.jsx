2734b106927af76bad503ae9f6d9f21f5f8eb9ee
Esha Patel <Patellesha11@gmail.com>
2025-09-03 09:54:40 +0300

    added

---
+++ frontend/src/pages/projectspage.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+
+const ProjectsPage = () => {
+  return (
+    <div>
+      <h1>Our Projects</h1>
+      <p>Full details about projects go here.</p>
+    </div>
+  );
+};
+
+export default ProjectsPage;   // ✅ must have this line
