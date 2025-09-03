35ddfc74cdfe72d8c39862ab590fcf83116155b5
Esha Patel <Patellesha11@gmail.com>
2025-09-03 08:46:29 +0300

    added section grids

--- frontend/src/pages/servicespage.jsx
+++ frontend/src/pages/servicespage.jsx
@@ -1,3 +1,26 @@
 import react from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+
+const ServicesPage = () => {
+    return (
+        <>
+        <Navbar />
+        <main className="services-pages">
+            <header className="page-header">
+                <h1>Our Services</h1>
+                <p>
+                    From resedential homes to commercial developments, we provide
+                    high-quality construction services tailored to your needs.
+                </p>
+            </header>
+
+            <section className="services-grid">
+                <div className="service-card">
+                    <h3></h3>
+                </div>
+            </section>
+        </main>
+        </>
+    )
+}
