28c4af5eb39136b27db4da1515c593e01d9b2741
Esha Patel <Patellesha11@gmail.com>
2025-09-03 15:54:21 +0300

    added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // only if you actually have it
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
