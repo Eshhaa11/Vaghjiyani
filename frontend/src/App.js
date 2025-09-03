c37f275dfa0c50fabbe01739f366a7a4a1cb30c1
Esha Patel <Patellesha11@gmail.com>
2025-09-03 09:13:00 +0300

    added projects page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,3 +2,4 @@ import react from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import servicespage from "./pages/servicespage"
+import projectspage from "./pages/projectspage"
