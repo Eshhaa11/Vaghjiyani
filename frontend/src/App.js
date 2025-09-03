808a2dfae72de4021793216193ec8df6c9d9f0cd
Esha Patel <Patellesha11@gmail.com>
2025-09-03 09:17:44 +0300

    added route path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/" element={<servicespage />} />
-        
+        <Route path="/" element={<projectspage />} />
       </Routes>
     </Router>
   );
