819ce4d5e8764ac473a9493349a191151a0c5ae3
Esha Patel <Patellesha11@gmail.com>
2025-09-03 09:16:43 +0300

    imported roujte path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,3 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import servicespage from "./pages/servicespage"
 import projectspage from "./pages/projectspage"
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<servicespage />} />
+        
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
