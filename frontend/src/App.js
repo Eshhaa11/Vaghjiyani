6620fa92a25b0a36b8e927ad68878a4f0c0786a5
Esha Patel <Patellesha11@gmail.com>
2025-09-03 09:10:54 +0300

    imported react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,4 @@
-import React from "react";
+import react from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-
-function App() {
-  return <Home />;
-}
-
-export default App;
+import servicespage from "./pages/servicespage"
