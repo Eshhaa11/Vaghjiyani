c6a728455f7886475f9fa9aacf510871cef70ece
Esha Patel <Patellesha11@gmail.com>
2025-09-03 08:58:49 +0300

    added

--- frontend/src/pages/servicespage.jsx
+++ frontend/src/pages/servicespage.jsx
@@ -3,52 +3,53 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
 const ServicesPage = () => {
-    return (
-        <>
-        <Navbar />
-        <main className="services-pages">
-            <header className="page-header">
-                <h1>Our Services</h1>
-                <p>
-                    From resedential homes to commercial developments, we provide
-                    high-quality construction services tailored to your needs.
-                </p>
-            </header>
+  return (
+    <>
+      <Navbar />
+      <main className="services-pages">
+        <header className="page-header">
+          <h1>Our Services</h1>
+          <p>
+            From resedential homes to commercial developments, we provide
+            high-quality construction services tailored to your needs.
+          </p>
+        </header>
 
-            <section className="services-grid">
-                <div className="service-card">
-                    <h3>🏠 Resedential Construction</h3>
-                    <p>
-                        We build custom homes, apartments, and housing developments with 
-                        modern design and durable materials.
-                    </p>
-                </div>
+        <section className="services-grid">
+          <div className="service-card">
+            <h3>🏠 Resedential Construction</h3>
+            <p>
+              We build custom homes, apartments, and housing developments with
+              modern design and durable materials.
+            </p>
+          </div>
 
-                <div className="services-card">
-                    <h3>🏢 Commercial Projects</h3>
-                    <p>Our team delivers reliable commerical spaces - from offices and 
-                       retail shops to large-scale business complexes .
-                    </p>
-                </div>
+          <div className="services-card">
+            <h3>🏢 Commercial Projects</h3>
+            <p>
+              Our team delivers reliable commerical spaces - from offices and
+              retail shops to large-scale business complexes .
+            </p>
+          </div>
 
-                <div className="services-card">
-                    <h3>🔨 Renovations & Remodeling</h3>
-                    <p>
-                        Transform your existing spaces with our professional renovation
-                        and remodeling solutions.
-                    </p>
-                </div>
+          <div className="services-card">
+            <h3>🔨 Renovations & Remodeling</h3>
+            <p>
+              Transform your existing spaces with our professional renovation
+              and remodeling solutions.
+            </p>
+          </div>
 
-                
-                <div className="services-card">
-                    <h3>🌉 Infrastrucutre & Civil Works</h3>
-                    <p>
-                        We contribute to city development through bridges, roads, and other major infrastructure projects.
-                    </p>
-                    <p></p>
-                </div>
-            </section>
-        </main>
-        </>
-    )
-}
+          <div className="services-card">
+            <h3>🌉 Infrastrucutre & Civil Works</h3>
+            <p>
+              We contribute to city development through bridges, roads, and
+              other major infrastructure projects.
+            </p>
+            <p></p>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
