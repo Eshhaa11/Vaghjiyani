c6bb6acfc43f2fb03d38dc2677866b3994e9af33
Esha Patel <Patellesha11@gmail.com>
2025-09-04 11:47:37 +0300

    added

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/about.css"
 import { Link } from "react-router-dom";
+import aboutImg from "../assets/about.jpg";
 
 const About = () => {
     return (
-        <section className="about" id="about">
+        <section className="about">
+            <div className="about-content">
             <h2>About Us</h2>
             <p>
                 With decades of experience, Vaghjiyani Enterprises Ltd delivers
@@ -12,7 +14,12 @@ const About = () => {
                 We pride ourselves on quality, safety, and innovation.
             </p>
             <Link to="/about" className="read-more">Read More</Link>
+            </div>
+            <div className="about-image">
+                <img src={aboutImg} alt="About our Company" />
+            </div>
         </section>
+
     );
 };
 
