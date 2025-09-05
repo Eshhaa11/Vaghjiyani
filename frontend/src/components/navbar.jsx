194e2907eb2b50b67d3767c6198a6f599edaf1ac
Esha Patel <Patellesha11@gmail.com>
2025-09-05 08:33:55 +0300

    added

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
-import logo from "../assets/logo.jpg";  // <-- import your logo
+import logo from "../assets/logo.jpg";  
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="navbar">
-      {/* Logo + Text */}
       <div className="logo">
         <Link to="/" className="logo-link">
           <img src={logo} alt="Vaghjiyani Logo" className="logo-img" />
@@ -19,7 +18,7 @@ const Navbar = () => {
       {/* Links */}
       <ul className={`nav-links ${isOpen ? "open" : ""}`}>
         <li><Link to="/">Home</Link></li>
-        <li><a href="#about">About</a></li>
+        <li><Link to="/about-us">About Us</Link></li>
         <li><Link to="/services">Services</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><a href="#testimonials">Testimonials</a></li>
