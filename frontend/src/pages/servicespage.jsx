5f8cfe9a6137a4bf9f307e14088488830357154d
Esha Patel <Patellesha11@gmail.com>
2025-09-05 11:40:13 +0300

    const all services

--- frontend/src/pages/servicespage.jsx
+++ frontend/src/pages/servicespage.jsx
@@ -8,7 +8,11 @@ import "../styles/services.css";
 const ServicesPage = () => {
   useEffect(() => {
     AOS.init({duration: 1000, once: true});
-  })
+  }, []);
+
+  const allServices = [
+    
+  ]
 };
 
 export default ServicesPage;
