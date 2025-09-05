0a68e5842c4cf8c5c982fb5b6457edfd8fe1db0c
Esha Patel <Patellesha11@gmail.com>
2025-09-05 10:56:24 +0300

    added const services

--- frontend/src/components/services.jsx
+++ frontend/src/components/services.jsx
@@ -2,26 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/services.css";
 
-const Services = () => {
-  return (
-    <section className="services" id="services">
-      <h2>Our Services</h2>
-      <p>
-        We provide professional construction services, including residential,
-        commercial, and infrastructure projects.
-      </p>
-      <ul className="service-list">
-        <li>🏠 Residential Construction</li>
-        <li>🏢 Commercial Projects</li>
-        <li>🔨 Renovations</li>
-        <li>🌉 Civil Works</li>
-      </ul>
-
-      <Link to="/services" className="btn">
-        See more
-      </Link>
-    </section>
-  );
-};
-
-export default Services;
+const services = () => {
+  
+}
