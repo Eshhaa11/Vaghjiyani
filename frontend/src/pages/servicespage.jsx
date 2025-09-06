7ce1253de980040df1145cc434fdda5d0d3f5fc6
Esha Patel <Patellesha11@gmail.com>
2025-09-06 08:51:04 +0300

    imported services.css

--- frontend/src/pages/servicespage.jsx
+++ frontend/src/pages/servicespage.jsx
@@ -1,3 +1,6 @@
 import react, {useEffect} from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import "../stylesservices.css";
