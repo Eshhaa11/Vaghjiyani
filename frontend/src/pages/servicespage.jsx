d402b40b4722390aac0c4835366855465ff60c1a
Esha Patel <Patellesha11@gmail.com>
2025-09-06 08:55:51 +0300

    added title

--- frontend/src/pages/servicespage.jsx
+++ frontend/src/pages/servicespage.jsx
@@ -3,10 +3,16 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import "../stylesservices.css";
+import "../styles/services.css";
 
 const ServicesPage = () => {
   useEffect(() => {
     AOS.init({duration: 1000, once: true});
-  })
+  }, []);
+
+  const allServices = [
+    {
+      title: "Resedntial Construction",
+    }
+  ]
 }
