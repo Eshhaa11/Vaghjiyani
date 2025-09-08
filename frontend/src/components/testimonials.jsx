a7075336a01506fc753748b7d39433dd05e7db9c
Esha Patel <Patellesha11@gmail.com>
2025-09-08 10:48:51 +0300

    imported styles

--- frontend/src/components/testimonials.jsx
+++ frontend/src/components/testimonials.jsx
@@ -3,3 +3,4 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import {Pagination, Autoplay} from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import "../styles/testimonials.css";
