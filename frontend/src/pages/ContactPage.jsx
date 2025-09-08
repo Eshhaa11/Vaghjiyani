cc51c3411082222ca8225362de1a339735f2e7a8
Esha Patel <Patellesha11@gmail.com>
2025-09-08 14:48:49 +0300

    added textarea

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -22,6 +22,8 @@ const ContactPage  = () => {
                 <form className="contact-form">
                     <input type="text" placeholder="Your Name" required/>
                     <input type="email" placeholder="Your Email" required/>
+                    <textarea placeholder="Your Message" rows="5" required></textarea>
+                    
                 </form>
             </div>
         </section>
