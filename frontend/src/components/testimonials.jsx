2c5600f053a6812e1c5f207919bc161a42f931a2
Esha Patel <Patellesha11@gmail.com>
2025-09-08 10:45:39 +0300

    imported react

--- frontend/src/components/testimonials.jsx
+++ frontend/src/components/testimonials.jsx
@@ -1,16 +1,1 @@
-import React from "react";
-import "../styles/testimonials.css";
-
-const Testimonials = () => {
-    return (
-    <section className="testimonials" id="testimonials">
-        <h2>What our Clients Say</h2>
-        <blockquote>
-            "Vaghjiyani completed our office project on time and with top-notch quality. Highly recommend!"
-        </blockquote>
-        <cite>- Happy Client</cite>
-    </section>
-    );
-};
-
-export default Testimonials;
+import react from "react";
