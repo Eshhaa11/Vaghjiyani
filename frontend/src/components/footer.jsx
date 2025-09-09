fb864c5ac712b070a97b5310db499655f8cedf5c
Esha Patel <Patellesha11@gmail.com>
2025-09-09 10:41:52 +0300

    added div

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -6,7 +6,9 @@ import "../styles/footer.css";
 const Footer = () => {
    return (
     <footer className="footer">
-        
+        <div className="footer-container">
+            
+        </div>
     </footer>
    ) 
 }
