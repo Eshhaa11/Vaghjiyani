0d4f2741c3c9e2c187db3f2191a8bea2765ff592
Esha Patel <Patellesha11@gmail.com>
2025-09-10 11:04:06 +0300

    added pagination keyboard

--- frontend/src/components/testimonials.jsx
+++ frontend/src/components/testimonials.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Autoplay, Navigation} from "swiper/modules";
+import { Pagination, Autoplay, Navigation, Keyboard} from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
