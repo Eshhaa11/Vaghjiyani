a4fa1b17590672bf0256ae59b4126a04f4707417
Esha Patel <Patellesha11@gmail.com>
2025-09-10 11:34:27 +0300

    added

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "../styles/footer.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import logoo from "../assets/logoo.jpg";
 
 const Footer = () => {
   useEffect(() => {
@@ -16,6 +17,7 @@ const Footer = () => {
         <div className="footer-brand" data-aos="fade-up">
           <h2>Vaghjiyani Enterprises Ltd</h2>
           <p>Excellence in construction, engineering, and infrastructure.</p>
+          <img src={logoo} alt="Vaghjiyani Logo" className="footer-logo" />
         </div>
 
         {/* Quick Links */}
@@ -33,12 +35,12 @@ const Footer = () => {
         {/* Contact Info */}
         <div className="footer-contact" data-aos="fade-up" data-aos-delay="300">
           <h4>Contact Us</h4>
-          <p>19th Floor, 4th Avenue Tower, < br /> 4th Avenue Ngong Upper Hill, Nairobi, Kenya</p>
-          <p>Email: info@vaghjiyani.com</p>
-          <p>Phone: +254 712 345 678</p>
+          <p>19th Floor, 4th Avenue Tower, < br /> 4th Avenue Ngong Upper Hill, < br /> Nairobi, Kenya</p>
+          <p>Email: info@vaghjiyani.co.ke</p>
+          <p>Phone:  +(254) 792-746-363 </p>
         </div>
       </div>
-
+      
       {/* Bottom bar */}
       <div className="footer-bottom" data-aos="fade-up" data-aos-delay="450">
         <p>© {new Date().getFullYear()} Vaghjiyani Enterprises Ltd. All rights reserved.</p>
